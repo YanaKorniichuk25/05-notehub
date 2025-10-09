a85b3bb63b9fc69fbc95a9a093fe967fb52e0c6d
YanaKorniichuk25 <149394343+YanaKorniichuk25@users.noreply.github.com>
2025-10-09 16:04:32 +0200

    Update noteService and add new token

--- src/services/noteService.ts
+++ src/services/noteService.ts
@@ -3,8 +3,6 @@ import axios from "axios";
 import toast from "react-hot-toast";
 import type { Note } from "../types/note";
 
-const token = import.meta.env.VITE_NOTEHUB_TOKEN;
-
 export type NotesResponse = {
   notes: Note[];
   totalPages: number;
@@ -12,19 +10,28 @@ export type NotesResponse = {
 
 axios.defaults.baseURL = "https://notehub-public.goit.study/api/";
 
+const getToken = (): string => {
+  const token = import.meta.env.VITE_NOTEHUB_TOKEN;
+  if (!token) {
+    throw new Error("Missing VITE_NOTEHUB_TOKEN");
+  }
+  return token;
+};
+
 export const fetchNotes = async (
   page = 1,
   search = ""
 ): Promise<NotesResponse> => {
   try {
+    const token = getToken();
     const response = await axios.get<NotesResponse>("notes", {
       params: { page, perPage: 12, search },
       headers: { accept: "application/json", Authorization: `Bearer ${token}` },
     });
     return response.data;
-  } catch {
+  } catch (error) {
     toast.error("Could not load notes");
-    throw new Error("Could not load notes");
+    throw error;
   }
 };
 
@@ -34,6 +41,7 @@ export const createNote = async (data: {
   tag: string;
 }): Promise<Note> => {
   try {
+    const token = getToken();
     const response = await axios.post<Note>("notes", data, {
       headers: {
         accept: "application/json",
@@ -43,22 +51,23 @@ export const createNote = async (data: {
     });
     toast.success("New note saved!");
     return response.data;
-  } catch {
+  } catch (error) {
     toast.error("Error creating note");
-    throw new Error("Error creating note");
+    throw error;
   }
 };
 
 export const removeNote = async (id: string): Promise<Note> => {
   try {
-    const response = await axios.delete<Note>(`/notes/${id}`, {
+    const token = getToken();
+    const response = await axios.delete<Note>(`notes/${id}`, {
       headers: { accept: "application/json", Authorization: `Bearer ${token}` },
     });
     toast.success("Note removed");
     return response.data;
-  } catch {
+  } catch (error) {
     toast.error("Error deleting note");
-    throw new Error("Error deleting note");
+    throw error;
   }
 };
 
